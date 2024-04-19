091f8d2e136161c8586e2fcfc6fe23ef1db637c2
Abhay S Gad <67232100+abhaygad@users.noreply.github.com>
2024-04-19 11:43:51 +0530

    Update home.py

--- home.py
+++ home.py
@@ -2,6 +2,6 @@ import streamlit as st
 def home():
     st.title("HOME")
     #st.subheader('We used to spend hours looking for insights in dozens of tools and reports. Now we login to one place to find out what costumers are doing and how to meet them where it matters most')
-    st.write("We serve customers by recommending the best 5g mobile phone currently available in the market, in terms of required specifications of the costumer and in different categories. The most obvious goal of a recommendation system is to recommend relevant products to the user")
-    st.write("Our website compares the mobile phones based on their performance in various categories like display , battery, CPU & graphic performances and finds our costumer the best smart phone based on their requirements.")
-    st.image("DATA/algo_flow.jpeg",caption="Alogrithum flow")
+    st.write("We serve customers by recommending the best 5G mobile phone currently available in the market, based on the required specifications of the customer and in different categories. The most obvious goal of a recommendation system is to recommend relevant products to the user.")
+    st.write("Our website compares mobile phones based on their performance in various categories like display, battery, CPU, and graphics performance, and finds our customer the best smartphone based on their requirements.")
+    st.image("DATA/algo_flow.jpeg",caption="Algorithm Flow")
